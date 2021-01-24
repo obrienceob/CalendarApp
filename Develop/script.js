f3608dbb1004aad8a40dd9444f4892a755946ef8
Conor O'Brien <obrienceob@gmail.com>
2021-01-24 14:41:50 -0800

    adding Develop folder with JS and stylesheet

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+//To Do list
+//display the current date
+//create timeblocks for the work day
+//each timeblock has event listeners for clicks to edit
+//color code timeblocks for time of day
+//save edits for local storage of events for the day
+//save is controlled by the save button
+//persistent local storage
+
