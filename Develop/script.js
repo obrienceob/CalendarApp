e845b516b25601d67f3dd9dbde078e85bb3d9232
Conor O'Brien <obrienceob@gmail.com>
2021-01-24 18:11:30 -0800

    cleaned up file

--- Develop/script.js
+++ Develop/script.js
@@ -16,39 +16,39 @@ $(document).ready(function() {
     
     //creating timeblock variables, to populate the hour into the block
     var time1 = pastTime.add(0, "h");
-    time1 = time1.format("h a");
+    time1 = time1.format("hh:mm a");
     $(".block1").text(time1);
     
     var time2 = pastTime.add(1, "h");
-    time2 = time2.format("h a");
+    time2 = time2.format("hh:mm a");
     $(".block2").text(time2);
 
     var time3 = pastTime.add(1, "h");
-    time3 = time3.format("h a");
+    time3 = time3.format("hh:mm a");
     $(".block3").text(time3);
 
     var time4 = pastTime.add(1, "h");
-    time4 = time4.format("h a");
+    time4 = time4.format("hh:mm a");
     $(".block4").text(time4);
 
     var time5 = pastTime.add(1, "h");
-    time5 = time5.format("h a");
+    time5 = time5.format("hh:mm a");
     $(".block5").text(time5);
 
     var time6 = pastTime.add(1, "h");
-    time6 = time6.format("h a");
+    time6 = time6.format("hh:mm a");
     $(".block6").text(time6);
 
     var time7 = pastTime.add(1, "h");
-    time7 = time7.format("h a");
+    time7 = time7.format("hh:mm a");
     $(".block7").text(time7);
 
     var time8 = pastTime.add(1, "h");
-    time8 = time8.format("h a");
+    time8 = time8.format("hh:mm a");
     $(".block8").text(time8);
 
     var time9 = pastTime.add(1, "h");
-    time9 = time9.format("h a");
+    time9 = time9.format("hh:mm a");
     $(".block9").text(time9);
 
     function whatTime() {
